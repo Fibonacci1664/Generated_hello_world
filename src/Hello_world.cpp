668881517e091f077249e28608fdc8b6d01d446c
Fibonacci1664 <dave.green1664@yahoo.co.uk>
2019-05-25 13:35:08 +0100

    First commit

---
+++ src/Hello_world.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Hello_world.cpp
+// Author      : D_Green
+// Version     :
+// Copyright   : D-Green
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Another Hello World program, only in C++ this time!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
